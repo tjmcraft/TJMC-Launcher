cceb99f2b57c9ebfb0268922f491d2c59d8d4ed3
MakAndJo <makandjo@outlook.com>
2020-12-18 09:33:31 +0500

    fixes

--- app/assets/js/uicore.js
+++ app/assets/js/uicore.js
@@ -1,6 +1,7 @@
 const $                                      = require('jquery')
 const {ipcRenderer, remote, shell, webFrame} = require('electron')
 const LoggerUtil                             = require('./assets/js/loggerutil')
+const request                                = require('request')
 
 const loggerUICore             = LoggerUtil('%c[UICore]', 'color: #000668; font-weight: bold')
 const loggerAutoUpdater        = LoggerUtil('%c[AutoUpdater]', 'color: #000668; font-weight: bold')
@@ -39,13 +40,47 @@ document.addEventListener('readystatechange', function () {
                 document.activeElement.blur()
             })
         })
-
-        // Remove focus from social media buttons once they're clicked.
-        Array.from(document.getElementsByClassName('mediaURL')).map(val => {
-            val.addEventListener('click', e => {
-                document.activeElement.blur()
-            })
-        })
-
     }
 });
+
+getVersionDataUrl('1.16.4').then(data => {
+    console.log(data);
+})
+
+    function loadMojangLauncherData(){
+        return new Promise((resolve, reject) => {
+            request.get('https://launchermeta.mojang.com/mc/launcher.json', (err, resp, body) => {
+                if(err){
+                    resolve(null)
+                } else {
+                    resolve(JSON.parse(body))
+                }
+            })
+        })
+    }
+
+    function getVersionDataUrl(version){
+        return new Promise((resolve, reject) => {
+            request('https://launchermeta.mojang.com/mc/game/version_manifest.json', (error, resp, body) => {
+                if(error){
+                    reject(error)
+                } else {
+                    const manifest = JSON.parse(body)
+
+                    for(let v of manifest.versions){
+                        if(v.id === version){
+                            resolve(v.url)
+                        }
+                    }
+
+                    resolve(null)
+                }
+            })
+        })
+    }
+
+    function addVer(select, val){
+        option = document.createElement( 'option' );
+        option.value = option.text = val;
+        select.add( option );
+    }
