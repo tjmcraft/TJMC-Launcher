e9f0ac254edcbe62a591bf3b2dba137b197eced0
MakAndJo <makandjo@outlook.com>
2020-12-30 17:54:48 +0500

    message ui core fixes

---
+++ app/assets/js/message.js
@@ -0,0 +1,146 @@
+const fs = require('fs');
+const { param } = require('jquery');
+
+class message {
+    constructor(params) {
+        if (document.querySelector('#overlay')) {
+            document.querySelector('#overlay').toggle();
+            setTimeout(() => {
+                document.querySelector('#overlay').remove();
+            }, 1000);
+        }
+
+        this.overlay = this.createElementWithId('div', 'overlay');
+        this.overlay.toggle(false);
+
+        let container = this.createElementWithId('div', 'container');
+        container.onclick = (event) => { event.stopPropagation(); };
+
+        if (params.closeButton) {
+            let tools = this.createToolsContainer();
+            tools.querySelector('#overlayCloseButton').onclick = () => {
+                this.destroy();
+            };
+            container.append(tools);
+        }
+
+        if (params.type) {
+            let path = null;
+            switch (params.type) {
+                case 'info':
+                    path = './app/assets/css/info-circle.svg';
+                    break;
+                case 'error':
+                    path = './app/assets/css/error-circle.svg';
+                    break;
+                case 'warn':
+                    path = './app/assets/css/warn-circle.svg';
+                    break;
+                case 'success':
+                    path = './app/assets/css/success-circle.svg';
+                    break;
+                default:
+                    path = '';
+                    break;
+            }
+
+            let data = fs.readFileSync(path, 'utf8');
+
+            let img = document.createElement('h2');
+            img.innerHTML = data;
+
+            container.append(img);
+        }
+
+        if (params.header) {
+            let h = document.createElement('h1');
+            h.innerText = params.header;
+            container.append(h);
+        }
+
+        if (params.text) {
+            let p = document.createElement('p');
+            p.innerText = params.text;
+            container.append(p);
+        }
+
+        if (params.okButton) {
+            let button = this.createElementWithClass('button', 'okButton');
+            button.innerText = 'Ок';
+            button.onclick = () => {
+                if (params.callback && typeof params.callback === 'function') {
+                    params.callback();
+                }
+                this.destroy();
+            };
+            container.append(button);
+        }
+
+        this.overlay.onclick = (e) => {
+            this.destroy();
+        };
+
+        document.onkeydown = (evt) => {
+            evt = evt || window.event;
+            let isEscape = false;
+            if ("key" in evt) {
+                isEscape = (evt.key === "Escape" || evt.key === "Esc");
+            } else {
+                isEscape = (evt.keyCode === 27);
+            }
+            if (isEscape) {
+                this.destroy();
+            }
+        };
+
+        this.overlay.append(container);
+
+        document.body.appendChild(this.overlay);
+        setTimeout(() => {
+            this.overlay.toggle(true);
+        });
+
+        return this.overlay;
+    }
+
+    createElementWithId(el, id) {
+        let e = document.createElement(el);
+        e.id = id;
+        return e;
+    }
+
+    createElementWithClass(el, cl) {
+        let e = document.createElement(el);
+        e.className = cl;
+        return e;
+    }
+
+    createToolsContainer() {
+        let tools = this.createElementWithClass('div', 'tools');
+        let cont = this.createElementWithClass('div', 'container');
+
+        let overlayCloseButton = this.createElementWithId('div', 'overlayCloseButton');
+
+        let img = document.createElement('img');
+        img.src = "./assets/css/cross.svg";
+
+        let keycode = this.createElementWithClass('div', 'keycode');
+        keycode.innerText = 'ESC';
+
+        overlayCloseButton.append(img);
+        cont.innerHTML += overlayCloseButton.outerHTML + keycode.outerHTML;
+
+        tools.append(cont);
+
+        return tools;
+    }
+
+    destroy() {
+        this.overlay.toggle(false);
+        setTimeout(() => {
+            this.overlay.remove();
+        }, 500);
+    }
+}
+
+module.exports = message
--- app/assets/js/uicore.js
+++ app/assets/js/uicore.js
@@ -1,11 +1,11 @@
-const $                                      = require('jquery')
 const {ipcRenderer, remote, shell, webFrame} = require('electron')
-const LoggerUtil                             = require('./assets/js/loggerutil')
-const request                                = require('request')
-const fs                                     = require('fs')
-const path                                   = require('path')
-const {Minecraft}                            = require('./assets/js/Minecraft')
-const client                               = require('./assets/js/launcher')
+const LoggerUtil = require('./assets/js/loggerutil')
+const Message = require('./assets/js/message')
+const request = require('request')
+const fs = require('fs')
+const path = require('path')
+const {Minecraft} = require('./assets/js/Minecraft')
+const client = require('./assets/js/launcher')
 
 const logg = LoggerUtil('%c[UICore]', 'color: #00aeae; font-weight: bold')
 
@@ -108,146 +108,5 @@ Element.prototype.toggle = function(s = null) {
         cl.remove(c)
     } else {
         cl.add(c)
-    }
-}
-
-
-
-class overlay {
-    constructor (header = null, text = null, type = null, closeButton = null, okButton = null) {
-        if (document.querySelector('#overlay')) {
-            document.querySelector('#overlay').toggle() 
-            setTimeout(()=>{
-                document.querySelector('#overlay').remove()
-            }, 1000)
-        }
-
-        this.overlay = this.createElementWithId('div', 'overlay')
-        this.overlay.toggle(false)
-
-        let container = this.createElementWithId('div', 'container')
-        container.onclick = (event) => {event.stopPropagation()}
-
-        if (closeButton) {
-            let tools = this.createToolsContainer(null, null)
-            tools.querySelector('#overlayCloseButton').onclick = () => {
-                this.destroy()
-            }
-            container.append(tools)
-        }
-
-        if (type) {
-            let path = null
-            switch (type) {
-                case 'info':
-                    path = './app/assets/css/info-circle.svg'
-                    break
-                case 'error':
-                    path = './app/assets/css/error-circle.svg'
-                    break
-                case 'warn':
-                    path = './app/assets/css/warn-circle.svg'
-                    break
-                case 'success':
-                    path = './app/assets/css/success-circle.svg'
-                    break
-                default:
-                    path = ''
-                    break                
-            }
-            
-            let data = fs.readFileSync(path, 'utf8')
-                
-            let img = document.createElement('h2')
-            img.innerHTML = data
-            
-            container.append(img)
-        }
-
-        if (header){
-            let h = document.createElement('h1')
-            h.innerText = header
-            container.append(h)
-        }
-
-        if (text) {
-            let p = document.createElement('p')
-            p.innerText = text
-            container.append(p)
-        }
-
-        if (okButton) {
-            let button = this.createElementWithClass('button', 'okButton')
-            button.innerText = 'Ок'
-            button.onclick = () => {
-                this.destroy()
-            }
-            container.append(button)
-        }
-
-        this.overlay.onclick = (e) => {
-            this.destroy()
-        }
-
-        document.onkeydown = (evt) => {
-            evt = evt || window.event
-            let isEscape = false
-            if ("key" in evt) {
-                isEscape = (evt.key === "Escape" || evt.key === "Esc")
-            } else {
-                isEscape = (evt.keyCode === 27)
-            }
-            if (isEscape) {
-                this.destroy()
-            }
-        }
-
-        this.overlay.append(container)
-
-        document.body.appendChild(this.overlay)
-        setTimeout(() => {
-            this.overlay.toggle(true)
-        })
-        
-        return this.overlay
-    }
-
-    createElementWithId (el, id) {
-        let e = document.createElement(el)
-        e.id = id
-        return e
-    }
-
-    createElementWithClass (el, cl) {
-        let e = document.createElement(el)
-        e.className = cl
-        return e
-    }
-
-    createToolsContainer (image = null, key = null) {
-        let tools = this.createElementWithClass('div', 'tools')
-        let cont = this.createElementWithClass('div', 'container')
-
-        let overlayCloseButton = this.createElementWithId('div', 'overlayCloseButton')
-
-        let img = document.createElement('img')
-        img.src = "./assets/css/cross.svg"
-
-        let keycode = this.createElementWithClass('div', 'keycode')
-        keycode.innerText = 'ESC'
-
-        overlayCloseButton.append(img)
-        cont.innerHTML += overlayCloseButton.outerHTML + keycode.outerHTML
-
-        tools.append(cont)
-        
-        return tools
-    }
-
-    destroy () {
-        this.overlay.toggle(false)
-        setTimeout(() => {
-            this.overlay.remove();
-        }, 500)
     }
 }
