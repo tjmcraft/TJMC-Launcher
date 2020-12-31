359e0351583ef48bc9a22cbfff62e6d12ed8f2bf
MakAndJo <makandjo@outlook.com>
2020-12-31 17:50:25 +0500

    some fixes

--- app/assets/js/uicore.js
+++ app/assets/js/uicore.js
@@ -12,6 +12,34 @@ const logg = LoggerUtil('%c[UICore]', 'color: #00aeae; font-weight: bold')
 document.addEventListener('readystatechange', function () {
     const window = remote.getCurrentWindow()
     if (document.readyState === 'interactive'){
+        if (window.isFullScreen()) {document.body.classList.add('fullscreen')}
+        window.on('enter-full-screen', (e, cmd) => {
+            document.body.classList.add('fullscreen')
+            new Message({
+                header:'Уведомление', 
+                text:"Вы вошли в полноэкранный режим", 
+                type:'info', 
+                closeButton: false, 
+                buttons: [
+                    {
+                        class: 'primary-button',
+                        name: 'Ok',
+                        closeOverlay: true
+                    },
+                    {
+                        class: 'default-button',
+                        name: 'Выйти',
+                        closeOverlay: true,
+                        callback: function () {
+                            window.setFullScreen(false)
+                        }
+                    }
+                ]
+            });
+        })
+        window.on('leave-full-screen', (e, cmd) => {
+            document.body.classList.remove('fullscreen')
+        })
 
         /* ================================= */
         const versionList = document.querySelector('#version')
@@ -91,10 +119,12 @@ document.addEventListener('readystatechange', function () {
             })
             
         }
+    } else if (document.readyState === 'complete'){
+        setTimeout(() => {
+            document.body.classList.remove('preload')
+        }, 1000)
     }
-});
-
-
+})
 
 
 /**
