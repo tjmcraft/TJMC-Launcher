6fd3563a481f3dec62537e0d161afacb6518c60b
MakAndJo <68428951+MakAndJo@users.noreply.github.com>
2021-01-02 22:36:36 +0500

    build fixes

--- build.js
+++ build.js
@@ -19,9 +19,9 @@ builder.build({
     targets: (process.argv[2] != null && Platform[process.argv[2]] != null ? Platform[process.argv[2]] : getCurrentPlatform()).createTarget(),
     config: {
         appId: 'tjmc-launcher',
-        productName: 'Helios Launcher',
+        productName: 'TJMC-Launcher',
         artifactName: '${productName}-setup-${version}.${ext}',
-        copyright: 'Copyright © 2018-2020 Daniel Scalzi',
+        copyright: 'Copyright © 2020-2021 MakAndJo',
         directories: {
             buildResources: 'build',
             output: 'dist'
@@ -46,8 +46,8 @@ builder.build({
         },
         linux: {
             target: 'AppImage',
-            maintainer: 'Daniel Scalzi',
-            vendor: 'Daniel Scalzi',
+            maintainer: 'MakAndJo',
+            vendor: 'MakAndJo',
             synopsis: 'Modded Minecraft Launcher',
             description: 'Custom launcher which allows users to join modded servers. All mods, configurations, and updates are handled automatically.',
             category: 'Game'
