5e9afc73ed474c7792df5b65b056beef407fcf84
MakAndJo <makandjo@outlook.com>
2021-04-20 16:24:12 +0500

    remove unnessasary files

--- app/assets/js/including.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const components = document.querySelectorAll('#include-component')
-
-const loadComponent = async (c) => {
-    c.outerHTML = await (await fetch(c.dataset.src)).text()
-}
-
-[...components].forEach(loadComponent)
