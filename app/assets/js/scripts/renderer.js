befe1d3dee3b533f9afe2f41d40c04200d6af8f3
MakAndJo <makandjo@outlook.com>
2021-06-29 21:25:45 +0500

    Update renderer.js

--- app/assets/js/scripts/renderer.js
+++ app/assets/js/scripts/renderer.js
@@ -31,10 +31,8 @@ var Installations
  * @param {String} next (requied) The ID of the next view container
  * @param {Number} currentFadeTime The fade out time for the current view
  * @param {Number} nextFadeTime The fade in time for the next view
- * @param {Function} onCurrentFade Callback function to execute when the current
- * view fades out.
- * @param {Function} onNextFade Callback function to execute when the next view
- * fades in.
+ * @param {Function} onCurrentFade Callback function to execute when the current view fades out.
+ * @param {Function} onNextFade Callback function to execute when the next view fades in.
  */
 export function switchView(next, currentFadeTime = 100, nextFadeTime = 100, onCurrentFade = () => {}, onNextFade = () => {}){
     let current = currentView
