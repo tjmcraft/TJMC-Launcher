6156b4b967aea1e9d449c14801ccaaa9aabf5bd3
MakAndJo <makandjo@outlook.com>
2021-06-29 23:14:02 +0500

    Update Tools.js

--- app/assets/js/scripts/Tools.js
+++ app/assets/js/scripts/Tools.js
@@ -14,14 +14,61 @@ export function getPreferredColorScheme() {
 }
 
 export async function getConfig() {
-    return await electron.invoke('configuration.get');
+    if (window.__STANDALONE__ && electron) {
+        return await electron.invoke('configuration.get');
+    } else {
+        return await fetchData('http://localhost:5248/configuration.get');
+    }
 }
 export async function setConfig(config) {
-    return await electron.invoke('configuration.set', config);
+    if (window.__STANDALONE__ && electron) {
+        return await electron.invoke('configuration.set', config);
+    } else {
+
+    }
 }
 export async function getMem() {
-    return await electron.invoke('system.mem');
+    if (window.__STANDALONE__ && electron) {
+        return await electron.invoke('system.mem');
+    } else {
+        return await fetchData('http://localhost:5248/system.mem');
+    }
 }
 export async function getInstallations() {
-    return await electron.invoke('installations.get');
+    if (window.__STANDALONE__ && electron) {
+        return await electron.invoke('installations.get')
+    } else {
+        return await fetchData('http://localhost:5248/installations.get');
+    }
+}
+export async function getGlobalVersions() {
+    if (window.__STANDALONE__ && electron) {
+        return await electron.invoke('versions.get.global');
+    } else {
+        return await fetchData('http://localhost:5248/versions.get.global');
+    }
+}
+export async function createInstallation(version, options) {
+    if (window.__STANDALONE__ && electron) {
+        return await electron.invoke('installations.create', version, options);
+    } else {
+        return await postData('http://localhost:5248/installations.create', { version, options });
+    }
+}
+
+async function fetchData(url = '') {
+    const response = await fetch(url);
+    return await response.json();
+}
+
+async function postData(url = '', data = {}) {
+    const response = await fetch(url, {
+        method: 'POST', 
+        cache: 'no-cache',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data) 
+    });
+    return await response.json();
 }
