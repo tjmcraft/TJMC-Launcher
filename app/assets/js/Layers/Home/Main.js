d7305b602f1c590fa9176073443bc90e74b7cef2
MakAndJo <makandjo@outlook.com>
2021-07-09 21:16:12 +0500

    Update Main.js

--- app/assets/js/Layers/Home/Main.js
+++ app/assets/js/Layers/Home/Main.js
@@ -21,7 +21,42 @@ export class Main {
                     ),
                     cE('div', { class: ['main-q'] },
                         cE('div', { class: ['section'] },
-                            cE('h3', null, 'Что происходит')
+                            cE('div', { class: ['grid-q'] },
+                                cE('div', { class: ['row-q', 'imageLeft', 'container-z']},
+                                    cE('img', {
+                                        class: ['featureImage', 'lim'],
+                                        src: 'https://discord.com/assets/46b2132c01604c9493d558de444929f4.svg'
+                                    }),
+                                    cE('div', { class: ['description'] },
+                                        cE('h2', null, 'This is image left box'),
+                                        cE('div', { class: ['subtitle', 'sizeQ'] }, 'Subtitle text for grid box. It can be longer and longer... But now it`s not'),
+                                    )
+                                )
+                            ),
+                            cE('div', { class: ['grid-q', 'grayBackground'] },
+                                cE('div', { class: ['row-q', 'imageRight', 'container-z']},
+                                    cE('img', {
+                                        class: ['featureImage', 'lim'],
+                                        src: 'https://discord.com/assets/46b2132c01604c9493d558de444929f4.svg'
+                                    }),
+                                    cE('div', { class: ['description'] },
+                                        cE('h2', null, 'This is image right box'),
+                                        cE('div', { class: ['subtitle', 'sizeQ'] }, 'Subtitle text for grid box. It can be longer and longer... But now it`s not'),
+                                    )
+                                )
+                            ),
+                            cE('div', { class: ['grid-q'] },
+                                cE('div', { class: ['row-q', 'imageBottom', 'container-z']},
+                                    cE('div', { class: ['description'] },
+                                        cE('h2', null, 'This is image bottom box'),
+                                        cE('div', { class: ['subtitle', 'sizeQ'] }, 'Subtitle text for grid box. It can be longer and longer... But now it`s not'),
+                                    ),
+                                    cE('img', {
+                                        class: ['featureImage'],
+                                        src: 'https://discord.com/assets/98ea5b9e92e304c7d352ac462996adc5.svg'
+                                    })
+                                )
+                            )
                         )
                     ),
                     cE('div', { class: ['footer-q'] }, 'footer'),
