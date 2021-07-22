d0ea901c907daf88aae926d3578e2041c350f208
MakAndJo <makandjo@outlook.com>
2021-07-23 01:04:08 +0500

    remove app folder

--- app/assets/js/scripts/Event.js
+++
@@ -1,9 +0,0 @@
-export class DOMEvent {
-    constructor(event, ...data) {
-        this.event = new CustomEvent(event, { ...data });
-    }
-    dispatchEvent(element = null) {
-        element = element || window.document;
-        element.dispatchEvent(this.event);
-    }
-}
