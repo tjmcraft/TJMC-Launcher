1fd7cd1af292291343a9059236daf968c55867f7
MakAndJo <68428951+MakAndJo@users.noreply.github.com>
2022-07-07 19:12:10 +0500

    Fix export mistake

--- src/main/managers/VersionManager.js
+++ src/main/managers/VersionManager.js
@@ -6,13 +6,13 @@ const request = require('request')
 const { merge, cleanObject } = require('../util/Tools')
 const logger = LoggerUtil('%c[VersionManager]', 'color: #0016d6; font-weight: bold')
 
-exports.getLocalVersions = async function() {
+exports.getLocalVersions = async function () {
     const dir_path = getVersionsDirectory();
     let ver_list = []
     try {
         fs.readdirSync(dir_path)?.forEach(folder => {
             const ver_path = path.join(dir_path, folder, folder + '.json')
-            if (fs.existsSync(ver_path)){
+            if (fs.existsSync(ver_path)) {
                 const file_c = JSON.parse(fs.readFileSync(ver_path, 'utf8'))
                 let version = {
                     name: file_c?.name || file_c?.id,
@@ -40,7 +40,7 @@ exports.getLocalVersions = async function() {
  * Gets Main JSON of given version
  * @param version Version of Minecraft
  */
-exports.getVersionManifest = async function(version, props = {}) {
+exports.getVersionManifest = async function (version, props = {}) {
     logger.debug('Loading Version JSON for: ' + version);
     const versionPath = path.join(getVersionsDirectory(), version);
     const versionJsonPath = path.join(versionPath, `${version}.json`);
@@ -48,7 +48,7 @@ exports.getVersionManifest = async function(version, props = {}) {
     if (fs.existsSync(versionJsonPath)) {
         c_version = JSON.parse(fs.readFileSync(versionJsonPath));
     } else {
-        const parsed = await exports.getGlobalVersions();
+        const parsed = await this.getGlobalVersions();
         for (const cv in parsed) {
             if (parsed[cv].id === version) {
                 c_version = JSON.parse(await downloadFile(parsed[cv].url || `https://tlauncher.ru/repo/versions/${version}.json`, true))
@@ -56,13 +56,13 @@ exports.getVersionManifest = async function(version, props = {}) {
         }
     }
     if (c_version.inheritsFrom) {
-        const inherit = await exports.getVersionManifest(c_version.inheritsFrom);
+        const inherit = await this.getVersionManifest(c_version.inheritsFrom);
         c_version.libraries = merge(c_version.libraries, inherit.libraries);
         c_version.mainClass = c_version.mainClass || inherit.mainClass;
         c_version.minecraftArguments = c_version.minecraftArguments || inherit.minecraftArguments;
         c_version.assetIndex = c_version.assetIndex || inherit.assetIndex;
         c_version.downloads = c_version.downloads || inherit.downloads;
-        if (c_version.arguments || inherit.arguments){
+        if (c_version.arguments || inherit.arguments) {
             c_version.arguments.game = c_version.arguments.game && inherit.arguments.game ? merge(c_version.arguments.game, inherit.arguments.game) : c_version.arguments.game || inherit.arguments.game
             c_version.arguments.jvm = c_version.arguments.jvm && inherit.arguments.jvm ? merge(c_version.arguments.jvm, inherit.arguments.jvm) : c_version.arguments.jvm || inherit.arguments.jvm
         }
@@ -74,12 +74,12 @@ exports.getVersionManifest = async function(version, props = {}) {
     return c_version
 }
 
-exports.removeVersion = async function(version) {
-    const versionFile = await exports.getVersionManifest(version)
+exports.removeVersion = async function (version) {
+    const versionFile = await this.getVersionManifest(version)
     const versionPath = path.join(getVersionsDirectory(), version)
     const assetsIndexDir = path.join(ConfigManager.getDataDirectory(), 'assets', 'indexes', `${versionFile.assetIndex.id}.json`)
-    fs.rmdirSync(versionPath, {recursive: true})
-    fs.rmdirSync(assetsIndexDir, {recursive: true})
+    fs.rmdirSync(versionPath, { recursive: true })
+    fs.rmdirSync(assetsIndexDir, { recursive: true })
     logger.log(`${version} was removed!`)
 }
 
@@ -92,24 +92,24 @@ exports.getGlobalVersionsManifests = async function () {
     return merge(m.versions, t.versions)
 }
 
-exports.updateGlobalVersionsConfig = async function() {
+exports.updateGlobalVersionsConfig = async function () {
     const dir_path = getVersionsDirectory();
     const manifest_path = path.join(dir_path, `version_manifest_v2.json`);
     const versions = await this.getGlobalVersionsManifests();
     fs.writeFileSync(manifest_path, JSON.stringify(versions, null, 4));
-    
+
 }
 
 exports.getGlobalVersions = async function () {
     const dir_path = getVersionsDirectory();
     const manifest_path = path.join(dir_path, `version_manifest_v2.json`);
-    if (!fs.existsSync(manifest_path)) await exports.updateGlobalVersionsConfig();
+    if (!fs.existsSync(manifest_path)) await this.updateGlobalVersionsConfig();
     const versions = JSON.parse(fs.readFileSync(manifest_path));
     return versions;
 }
 
 exports.getGlobalVersion = async function (version) {
-    const versions = await exports.getGlobalVersions();
+    const versions = await this.getGlobalVersions();
     return versions.find(e => e.id === version) || undefined;
 }
 
@@ -126,10 +126,9 @@ function downloadFile(url, retry = false) {
     return new Promise((resolve, reject) => {
         request(url, async (error, response, body) => {
             if (error) {
-                logger.debug(`Failed to download ${url} due to\n${error}.`+` Retrying... ${retry}`)
-                if (retry) {await downloadFile(url, false)} else reject(error);
-            } else 
-            if (response?.statusCode != 200) {
+                logger.debug(`Failed to download ${url} due to\n${error}.` + ` Retrying... ${retry}`)
+                if (retry) { await downloadFile(url, false) } else reject(error);
+            } else if (response?.statusCode != 200) {
                 reject('Invalid status code <' + response.statusCode + '>')
             } else resolve(body)
         })
