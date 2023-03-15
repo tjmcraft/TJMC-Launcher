afc4e99a61b5aed1d09793e2caa7c05ea36efc54
MakAndJo <68428951+MakAndJo@users.noreply.github.com>
2023-03-15 19:19:50 +0500

    Update tray

--- src/main/tray.js
+++ src/main/tray.js
@@ -9,49 +9,43 @@ const MainWindow = require("./MainWindow");
  */
 var tray = undefined;
 
-const createTray = async () => {
-  tray = new Tray(process.platform != "win32" ? platformIcon.resize({ width: 16, height: 16 }) : platformIcon);
-  //tray.on('right-click', toggleWindow)
-  //tray.on('double-click', toggleWindow)
-  process.platform === "win32" && tray.on('click', function (event) {
-      MainWindow.restore();
-  })
-  const menu = Menu.buildFromTemplate([
-      {
-          label: 'TJMC-Launcher',
-          icon: platformIcon.resize({ width: 16, height: 16 }),
-          enabled: process.platform != "win32",
-          click: () => process.platform != "win32" && MainWindow.restore(),
-      },
-      {
-          type: 'separator'
-      },
-      {
-          label: 'Settings',
-          click: () => {
-              MainWindow.restore();
-              MainWindow.send('open-settings');
-          }
-      },
-      {
-          label: 'Open Folder',
-          click: () => openDir(ConfigManager.getDataDirectory())
-      },
-      {
-          type: 'separator'
-      },
-      {
-          label: 'Quit',
-          click: () => app.quit()
-      }
-  ]);
-  tray.setContextMenu(menu);
-  tray.setToolTip("TJMC-Launcher");
-}
-
-const destroyTray = () => {
-  return tray && tray.destroy();
+exports.createTray = async () => {
+    tray = new Tray(process.platform != "win32" ? platformIcon.resize({ width: 16, height: 16 }) : platformIcon);
+    //tray.on('right-click', toggleWindow)
+    //tray.on('double-click', toggleWindow)
+    process.platform === "win32" && tray.on('click', function (event) {
+        MainWindow.restore();
+    })
+    const menu = Menu.buildFromTemplate([
+        {
+            label: 'TJMC-Launcher',
+            icon: platformIcon.resize({ width: 16, height: 16 }),
+            enabled: process.platform != "win32",
+            click: () => process.platform != "win32" && MainWindow.restore(),
+        },
+        {
+            type: 'separator'
+        },
+        {
+            label: 'Settings',
+            click: () => {
+                MainWindow.restore();
+                MainWindow.send('open-settings');
+            }
+        },
+        {
+            label: 'Open Folder',
+            click: () => openDir(ConfigManager.getDataDirectory())
+        },
+        {
+            type: 'separator'
+        },
+        {
+            label: 'Quit',
+            click: () => app.quit()
+        }
+    ]);
+    tray.setContextMenu(menu);
+    tray.setToolTip("TJMC-Launcher");
 };
-
-exports.createTray = createTray;
-exports.destroyTray = destroyTray;
+exports.destroyTray = () => tray != void 0 && tray.destroy();
