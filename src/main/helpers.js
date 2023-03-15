9479602e8632f43695b92e7dd4ab769fff60cd49
MakAndJo <68428951+MakAndJo@users.noreply.github.com>
2023-03-15 19:18:33 +0500

    Update helpers

--- src/main/helpers.js
+++ src/main/helpers.js
@@ -1,25 +1,23 @@
 const { shell, nativeImage } = require("electron");
 const path = require('path');
 
-exports.openDir = function(path) {
-  // logger.debug("[Main]", "{OpenMineDir}", "Path:", path);
-  shell.openPath(path);
+exports.openDir = function (path) {
+	shell.openPath(path);
 }
 
 exports.platformIcon = ((platform) => {
-  let ext, filename;
-  switch (platform) {
-      case "win32":
-          ext = "ico";
-          filename = "icon";
-          break;
-      default:
-          ext = "png";
-          filename = "icon";
-  }
-  console.debug(">>",process.resourcesPath)
-  const iconPath = path.join(__dirname, '../..', 'icons', `${filename}.${ext}`);
-  const image = nativeImage.createFromPath(iconPath);
-  image.setTemplateImage(true);
-  return image;
+	let ext, filename;
+	switch (platform) {
+		case "win32":
+			ext = "ico";
+			filename = "icon";
+			break;
+		default:
+			ext = "png";
+			filename = "icon";
+	}
+	const iconPath = path.join(__dirname, '../..', 'icons', `${filename}.${ext}`);
+	const image = nativeImage.createFromPath(iconPath);
+	image.setTemplateImage(true);
+	return image;
 })(process.platform);
