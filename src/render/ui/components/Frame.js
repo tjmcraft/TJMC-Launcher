41efd128b5a354f77e4e4ccdaffc5573e5c95f90
MakAndJo <68428951+MakAndJo@users.noreply.github.com>
2023-03-18 17:50:40 +0500

    Fix Frame svg

--- src/render/ui/components/Frame.js
+++ src/render/ui/components/Frame.js
@@ -28,7 +28,7 @@ export function Frame({ title = "" }) {
 					</div>
 					<div className={buildClassName(style.frameButton, style.close)} onClick={onClose}>
 						<svg aria-hidden="false" width="12" height="12" viewBox="0 0 12 12">
-							<polygon fill="currentColor" fill-rule="evenodd" points="11 1.576 6.583 6 11 10.424 10.424 11 6 6.583 1.576 11 1 10.424 5.417 6 1 1.576 1.576 1 6 5.417 10.424 1" ns="http://www.w3.org/2000/svg" />
+							<polygon fill="currentColor" fill-rule="evenodd" points="11 1.576 6.583 6 11 10.424 10.424 11 6 6.583 1.576 11 1 10.424 5.417 6 1 1.576 1.576 1 6 5.417 10.424 1" />
 						</svg>
 					</div>
 				</div>
