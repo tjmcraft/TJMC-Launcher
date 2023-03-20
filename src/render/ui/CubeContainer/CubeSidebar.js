08b917542136ef5bef3f473dbe4068dfc25e574f
MakAndJo <68428951+MakAndJo@users.noreply.github.com>
2023-03-20 20:23:01 +0500

    Update cube scrollers

--- src/render/ui/CubeContainer/CubeSidebar.js
+++ src/render/ui/CubeContainer/CubeSidebar.js
@@ -11,64 +11,58 @@ import InstanceItem from "./InstanceItem";
 
 
 const CubeSidebarItems = memo(() => {
-
 	const installations = useGlobal(global => Object.keys(selectInstallations(global)));
 	const currentHash = useGlobal(selectCurrentVersionHash);
-
-	if (!installations.length) return (
-		<div className={buildClassName('item', "d-flex", "centred", 'fp')}>
-			<h1>{'Добавьте версию'}</h1>
-		</div>
-	);
-
-	return (
+	return installations.length ? (
 		installations.map((hash) =>
 			createElement(CubeSidebarItem, {
 				key: hash,
 				hash,
 				isSelected: currentHash == hash,
 			}))
+	) : (
+		<div className={buildClassName('item', "d-flex", "centred", 'fp')}>
+			<h1>{'Добавьте версию'}</h1>
+		</div>
 	);
 });
 
-const InstanceItems = memo(() => {
-
-	const instances = useGlobal(global => Object.keys(selectInstances(global)));
-
-	if (!instances.length) return (
-		<div className={buildClassName('item', "d-flex", "centred", 'fp')}>
-			<h1>{'Launch new instance'}</h1>
+const InstallationsScroller = memo(() => {
+	const { openVersionChooserModal } = getDispatch();
+	const onClick = () => openVersionChooserModal();
+	return (
+		<div className={buildClassName('scroller', 'thin-s')}>
+			<h2 className={buildClassName('header-w', 'container-df')}>
+				<span>{"Версии"}</span>
+				<div className={'simple-button'} onClick={onClick}>{SVG('add-plus')}</div>
+			</h2>
+			<CubeSidebarItems />
 		</div>
 	);
+});
 
-	return instances.map((instanceId) =>
-		<InstanceItem
-			key={instanceId}
-			instanceId={instanceId}
-		/>
+const InstanceScroller = memo(() => {
+	const instances = useGlobal(global => Object.keys(selectInstances(global)));
+	return instances.length > 0 && (
+		<div className={buildClassName('scroller', 'thin-s')}>
+			<h2 className={buildClassName('header-w', 'container-df')}>
+				<span>{"Instances"}</span>
+			</h2>
+			{instances.map((instanceId) =>
+				<InstanceItem
+					key={instanceId}
+					instanceId={instanceId}
+				/>
+			)}
+		</div>
 	);
 });
 
 const CubeSidebar = () => {
-
-	const { openVersionChooserModal } = getDispatch();
-	const onClick = () => openVersionChooserModal();
-
 	return (
 		<Fragment>
-			<div className={buildClassName('scroller', 'thin-s')}>
-				<h2 className={buildClassName('header-w', 'container-df')}>
-					<span>{"Версии"}</span>
-					<div className={'simple-button'} onClick={onClick}>{SVG('add-plus')}</div>
-				</h2>
-				<CubeSidebarItems />
-			</div>
-			<div className={buildClassName('scroller', 'thin-s')}>
-				<h2 className={buildClassName('header-w', 'container-df')}>
-					<span>{"Instances"}</span>
-				</h2>
-				<InstanceItems />
-			</div>
+			<InstallationsScroller />
+			<InstanceScroller />
 		</Fragment>
 	);
 };
