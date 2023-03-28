5960b471f335756031663e3afa16e94dee8aac0c
MakAndJo <68428951+MakAndJo@users.noreply.github.com>
2023-03-28 14:46:38 +0500

    Getting works of creating directories

--- src/main/managers/JavaManager.js
+++ src/main/managers/JavaManager.js
@@ -1,9 +1,13 @@
 const child = require('child_process');
+const ConfigManager = require('../managers/ConfigManager');
 const { downloadFile } = require('../util/download');
 const LoggerUtil = require("../util/loggerutil");
+const path = require('path');
+const fs = require('fs');
 const logger = LoggerUtil('%c[JavaManager]', 'color: #beb600; font-weight: bold');
 
-const checkJava = function (java) {
+exports.checkJava = function (java) {
+  console.debug("Check java:", java);
   return new Promise(resolve => {
     let cmd = `"${java}" -version`;
     child.exec(cmd, {}, (error, stdout, stderr) => {
@@ -17,7 +21,35 @@ const checkJava = function (java) {
   })
 }
 
-exports.parseRecommendedJava = function (manifest) {
+const convertOSToJavaFormat = ElectronFormat => {
+  switch (ElectronFormat) {
+    case 'win32':
+      return 'windows';
+    case 'darwin':
+      return 'mac';
+    case 'linux':
+      return 'linux';
+    default:
+      return false;
+  }
+};
+
+const convertArchToJavaFormat = ElectronFormat => {
+  switch (ElectronFormat) {
+    case 'x64':
+      return 'x64';
+    case 'ia32':
+      return 'x32';
+    case 'arm':
+      return 'arm';
+    case 'arm64':
+      return 'aarch64';
+    default:
+      return false;
+  }
+};
+
+const parseRecommendedJava = function (manifest) {
   if (manifest.javaVersion) {
     return manifest.javaVersion;
   } else {
@@ -28,17 +60,51 @@ exports.parseRecommendedJava = function (manifest) {
   }
 }
 
-/**
- * Load Mojang's launcher.json file.
- *
- * @returns {Promise.<Object>} Promise which resolves to Mojang's launcher.json object.
- */
-const loadMojangLauncherData = function () {
+const fetchJavaConfig = function (javaVersionCode) {
+  const os = process.platform == "win32" ? "windows" : process.platform == "darwin" ? "mac-os" : "linux";
+  const arch = os == "linux" ? "" : ((os == "mac-os" ? (process.arch == "arm64" ? "arm64" : "") : (process.arch == "x64" ? "x64" : "x86")));
+  const pc = os + "-" + arch;
   return new Promise((resolve, reject) => {
-    downloadFile(`https://launchermeta.mojang.com/mc/launcher.json`)
-      .then(response => resolve(response))
-      .catch(e => resolve(null));
-  })
+    downloadFile("https://launchermeta.mojang.com/v1/products/java-runtime/2ec0cc96c44e5a76b9c8b7c39df7210883d12871/all.json").then((response) => {
+      if (!Object.keys(response).includes(pc)) return reject("Unsupported platform");
+      if (!response[pc][javaVersionCode]) return reject("Unsupported java version");
+      resolve(response[pc][javaVersionCode][0]);
+    })
+  });
+}
+
+const downloadJava = function (javaVersionCode) {
+  return new Promise((resolve, reject) => {
+    const javaPath = path.join(ConfigManager.getLauncherDirectory(), "java", javaVersionCode, "bin");
+    if (fs.existsSync(javaPath)) {
+      return resolve(javaPath);
+    } else {
+      fetchJavaConfig(javaVersionCode).then(async (config) => {
+        const manifest = await downloadFile(config.manifest.url);
+        const files = Object.keys(manifest.files).map((file) => {
+          return { name: file, downloads: manifest.files[file].downloads, type: manifest.files[file].type };
+        });
+        const directory = files.filter((file) => file.type == "directory");
+        const filesToDownload = files.filter((file) => file.type == "file");
+
+        let javaDirs = [ConfigManager.getLauncherDirectory(), "java", javaVersionCode];
+        javaDirs.forEach((dir, i) => {
+          let _dir = javaDirs.slice(0, i + 1).join(path.sep);
+          if (!fs.existsSync(_dir)) {
+            fs.mkdirSync(_dir);
+          }
+        });
+
+        directory.forEach((dir) => {
+          fs.mkdirSync(path.join(ConfigManager.getLauncherDirectory(), "java", javaVersionCode, dir.name));
+        });
+
+        console.debug(">", manifest);
+        console.debug(">>", files);
+        resolve(true);
+      });
+    }
+  });
 }
 
 /**
@@ -87,4 +153,8 @@ const latestAdoptium = function (major) {
   });
 }
 
-exports.checkJava = checkJava;
+exports.getRecommendedJava = async (manifest) => {
+  const recommendedJava = parseRecommendedJava(manifest);
+  await downloadJava(recommendedJava.component);
+  return undefined;
+}
