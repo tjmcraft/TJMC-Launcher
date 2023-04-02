7f99932df339075d747d59f5fbd7c5122d5427eb
MakAndJo <68428951+MakAndJo@users.noreply.github.com>
2023-04-02 12:19:20 +0500

    Update LaunchWorker

--- src/main/game/launch.worker.js
+++ src/main/game/launch.worker.js
@@ -8,6 +8,8 @@ const { parentPort, workerData, isMainThread } = require("node:worker_threads");
 
 if (!isMainThread) {
     if (!workerData) return;
+    const logger = LoggerUtil(`%c[LaunchWorker-${options.installation.hash}]`, 'color: #16be00; font-weight: bold');
+
     const options = Object.assign({}, workerData);
     options.overrides.path.gameDirectory = options.installation.gameDir || path.resolve(options.overrides.path.gameDirectory || options.overrides.path.minecraft);
     options.overrides.path.version = options.installation.versionPath || path.join(options.overrides.path.versions, options.installation.lastVersionId);
@@ -16,13 +18,11 @@ if (!isMainThread) {
         uuid: getOfflineUUID(options.auth.username)
     });
 
-    const logger = LoggerUtil(`%c[Launcher-${options.installation.hash}]`, 'color: #16be00; font-weight: bold');
+    logger.debug("Launcher compiled options:", options);
 
     const instance = new Minecraft(options);
     instance.on('progress', (e) => parentPort.postMessage({ type: 'progress', payload: e }));
     instance.on('download-progress', (e) => parentPort.postMessage({ type: 'download-progress', payload: e }));
-
-    logger.debug("Launcher compiled options:", options);
 
     (async () => {
         if (!fs.existsSync(options.overrides.path.version))
