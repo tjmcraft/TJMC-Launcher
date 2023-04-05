166ee2fb887c73e1a0a3dde670f3cd373af1c2dd
MakAndJo <68428951+MakAndJo@users.noreply.github.com>
2023-04-05 23:41:37 +0500

    Fixing offline auth

--- src/main/managers/AuthManager.js
+++ src/main/managers/AuthManager.js
@@ -2,33 +2,80 @@ const EventEmitter = require('node:events');
 const { downloadFile, postBody } = require('../util/download');
 const ConfigManager = require('./ConfigManager');
 const keytar = require('keytar');
+const { getOfflineUUID } = require('../util/Tools');
 
 const KEYTAR_KEY = 'ru.tjmc.launcher.auth';
 
 class AuthManager extends EventEmitter {
 
-	constructor() {
-		super();
-	}
+	/**
+	 * Current user identifier
+	 * @type {string}
+	 */
+	currentUserId = undefined;
 
+	/**
+	 * Current user type
+	 * @type {'offline'|'tjmc'}
+	 */
+	currentUserType = undefined;
+
+	/**
+	 * Current user name
+	 * @type {string}
+	 */
+	currentUserName = undefined;
+
+	/**
+	 * Current access token
+	 * @type {string}
+	 */
 	token = undefined;
 
+	constructor() {
+		super();
+		// this.currentUserType = this.currentUserId.split('/')[0];
+	}
+
 	load = async () => {
-		const username = ConfigManager.getOption('currentUser');
-		if (username) {
-			const storedToken = JSON.parse(await keytar.getPassword(KEYTAR_KEY, username));
+		this.currentUserId = ConfigManager.getOption('currentUser');
+		if (this.currentUserId) {
+			const storedToken = JSON.parse(await keytar.getPassword(KEYTAR_KEY, this.currentUserId));
 			if (storedToken) {
-				this.token = storedToken.accessToken;
+				if (this.currentUserId.split('/')[0] == 'tjmc.auth') {
+					this.token = storedToken.accessToken;
+				}
 			}
 		}
 	}
 
 	getCurrentUser = async (token = undefined) => {
 		token = token ?? this.token;
+		if (this.currentUserId && this.currentUserId.split('/')[0] == 'offline') {
+			return this.createMockedOfflineUser(this.currentUserId.split('/')[1]);
+		}
 		if (token == void 0) return;
 		const { response } = await downloadFile("https://app.tjmc.ru/api/user?access_token=" + token);
 		return response.user;
 	};
+
+	createMockedOfflineUser = (username) => {
+		const userId = `offline/${username}`;
+		const user = {
+			username: username,
+			realname: username,
+			"id": userId,
+			"avatar": undefined,
+			"email": undefined,
+			"discriminator": 1613,
+			"public_flags": "0",
+			"balance": 0,
+			"uuid": getOfflineUUID(username),
+			"permission": "default",
+			"permission_display_name": "Default"
+		};
+		return user;
+	}
 
 	revokeAuth = async (userId) => {
 
@@ -36,34 +83,14 @@ class AuthManager extends EventEmitter {
 
 	logoutCurrentUser = async () => {
 		ConfigManager.setOption('currentUser', '');
+		this.currentUserId = undefined;
 		this.token = undefined;
 	};
 
 	handleOfflineAuth = async (username) => {
-		const user = {
-			username: username,
-			realname: username,
-			"id": "offline.1",
-			"avatar": undefined,
-			"email": undefined,
-			"discriminator": 1613,
-			"public_flags": "0",
-			"balance": 0,
-			"uuid": "130ef346-8ed3-3c92-8783-01c0a3aea9d8",
-			"permission": "default",
-			"permission_display_name": "Default"
-		};
+		const user = this.createMockedOfflineUser(username);
 		try {
-			ConfigManager.setOption('currentUser', user.realname);
-			keytar.setPassword(KEYTAR_KEY, user.realname, JSON.stringify({
-				type: 'tjmc.auth',
-				accessToken: response.accessToken,
-				accessTokenExpiresAt: response.accessTokenExpiresAt,
-				refreshToken: response.refreshToken,
-				refreshTokenExpiresAt: response.refreshTokenExpiresAt,
-				scope: response.scope,
-				user: { id: response.user.id },
-			}));
+			ConfigManager.setOption('currentUser', user.id);
 		} catch (e) {}
 		if (user?.id) {
 			this.emit('user-switch', {
@@ -86,17 +113,19 @@ class AuthManager extends EventEmitter {
 		if (response.accessToken) {
 			this.token = response.accessToken;
 			const user = await this.getCurrentUser();
+			const userId = `tjmc/${user.realname}`;
 			try {
-				ConfigManager.setOption('currentUser', user.realname);
-				keytar.setPassword(KEYTAR_KEY, user.realname, JSON.stringify({
+				ConfigManager.setOption('currentUser', userId);
+				keytar.setPassword(KEYTAR_KEY, userId, JSON.stringify({
 					type: 'tjmc.auth',
 					accessToken: response.accessToken,
 					accessTokenExpiresAt: response.accessTokenExpiresAt,
 					refreshToken: response.refreshToken,
 					refreshTokenExpiresAt: response.refreshTokenExpiresAt,
 					scope: response.scope,
-					user: { id: response.user.id },
+					user: { id: response.user.id, username: user.realname },
 				}));
+				this.currentUserId = userId;
 			} catch (e) {}
 			if (user?.id) {
 				this.emit('user-switch', {
