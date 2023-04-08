b995ae93e21890ae1c11fecb36c7df64e8bb0be7
MakAndJo <68428951+MakAndJo@users.noreply.github.com>
2023-04-08 08:07:28 +0500

    Added auth transitions

--- src/render/ui/App.js
+++ src/render/ui/App.js
@@ -7,8 +7,10 @@ import useConstructor from "Hooks/useConstructor.js";
 import Preloader from "UI/Preloader.js";
 import Auth from "UI/Auth/Auth.js";
 import Main from "UI/Main.js";
+import useShowTransition from "Hooks/useShowTransition";
+import buildClassName from "Util/buildClassName";
 
-const AppContainer = memo(() => {
+const AppContainer = ({ isShown }) => {
 
 	const { openSettingsModal } = getDispatch();
 
@@ -17,27 +19,35 @@ const AppContainer = memo(() => {
 		electron.on('open-settings', (e, data) => openSettingsModal()); // Send global event to open settings [electron]
 	}, [openSettingsModal]);
 
-	return (
-		<div className="app-container">
+	const {
+		shouldRender,
+		transitionClassNames,
+	} = useShowTransition(
+		isShown, undefined, true, undefined, false, { }, 1350
+		);
+
+	return shouldRender && (
+		<div className={buildClassName("app-container",transitionClassNames)}>
 			<Main />
 			<div className="uploadArea" />
 		</div>
 	);
-});
+};
 
 const App = () => {
 
 	const { initHost } = getDispatch();
 	useConstructor(initHost);
 
-	const isAuthReady = useGlobal(global => global.auth_state == "ready");
+	const isAuthReady = useGlobal(global => ["ready"].includes(global.auth_state));
 
 	return (
 		<div className="app">
-			{isAuthReady ? <AppContainer /> : <Auth />}
+			<Auth isShown={!isAuthReady} />
+			<AppContainer isShown={isAuthReady} />
 			<Preloader />
 		</div>
 	);
 };
 
-export default memo(App);
+export default (App);
