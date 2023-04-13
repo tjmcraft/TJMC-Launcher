8921cd7ca1cc81a071ab82122c40148b7700f780
MakAndJo <68428951+MakAndJo@users.noreply.github.com>
2023-04-13 21:20:00 +0500

    Disable checkHash container when checkFiles off

--- src/render/ui/components/ToggleSwitch.js
+++ src/render/ui/components/ToggleSwitch.js
@@ -3,10 +3,10 @@ import { createElement, memo } from "react";
 import style from "CSS/toggleswitch.module.css";
 
 
-const ToggleSwitch = ({ id, checked, onChange }) => {
+const ToggleSwitch = ({ id, checked, onChange, disabled = false }) => {
 	return (
 		<label className={style.toggleSwitch}>
-			<input type="checkbox" id={id} checked={checked} onChange={onChange} />
+			<input type="checkbox" id={id} checked={checked} onChange={onChange} disabled={disabled} />
 			<span className={style.slider} />
 		</label>
 	);
