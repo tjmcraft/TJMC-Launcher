403b2f53aba0de543cb8ea061600a9e2f4b67e48
MakAndJo <68428951+MakAndJo@users.noreply.github.com>
2023-04-16 21:11:46 +0500

    Fix

--- src/render/ui/CubeContainer/CubeMainContainer.js
+++ src/render/ui/CubeContainer/CubeMainContainer.js
@@ -2,19 +2,19 @@ import { memo, createElement } from "react";
 import useGlobal from "Hooks/useGlobal";
 import { selectInstallation } from "Model/Selectors/installations";
 
-const Test = memo(({ hash }) => {
+const Test = ({ hash }) => {
 	const { name } = useGlobal(global => {
 		const version = selectInstallation(global, hash);
 		return { name: version.name };
 	}, [hash]);
 	return (<h1>{name} - {hash}</h1>);
-});
+};
 
 const CubeMainContainer = ({ hash }) => {
 
 	if (APP_ENV == "production") return null;
 	return (
-		<div class="main">
+		<div className="main">
 			<Test hash={hash} />
 		</div>
 	);
