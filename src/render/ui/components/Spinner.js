9489a6ca128f325c79875c9780e5f154213cef04
MakAndJo <68428951+MakAndJo@users.noreply.github.com>
2023-05-14 00:49:40 +0500

    Update Spinner component

--- src/render/ui/components/Spinner.js
+++ src/render/ui/components/Spinner.js
@@ -6,10 +6,16 @@ import './Spinner.css';
 
 
 const Spinner = ({
-	color = 'blue'
+	color = 'blue',
+	size = 1
 }) => {
+	const sizeClass = ({
+		1: "size1",
+		2: "size2",
+		3: "size3",
+	})[size];
 	return (
-		<div className={buildClassName("Spinner", color)}>
+		<div className={buildClassName("Spinner", color, sizeClass)}>
 			<div />
 		</div>
 	);
