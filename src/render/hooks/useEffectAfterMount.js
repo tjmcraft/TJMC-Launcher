c5fbfa8d536645863704f7e49a7627ba32a1f8f9
MakAndJo <68428951+MakAndJo@users.noreply.github.com>
2023-05-23 14:00:41 +0500

    Added update interval select

--- src/render/hooks/useEffectAfterMount.js
+++ src/render/hooks/useEffectAfterMount.js
@@ -6,11 +6,6 @@ export default function useEffectAfterMount(effect, deps = []) {
 	useEffect(() => {
 		if (isMounted.current) return effect();
 		else isMounted.current = true;
-	// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [effect, ...deps]);
-
-	// reset on unmount; in React 18, components can mount again
-	useEffect(() => {
-		isMounted.current = false;
-	});
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, deps);
 }
