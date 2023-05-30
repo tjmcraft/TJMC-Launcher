bad14f8e1364663600a0e09a9ebadb2479176406
MakAndJo <68428951+MakAndJo@users.noreply.github.com>
2023-05-31 00:39:31 +0500

    Added tooltip position hook

--- src/render/ui/components/Tooltip.js
+++ src/render/ui/components/Tooltip.js
@@ -1,22 +1,23 @@
-import { createElement, memo } from "react";
+import { createElement, forwardRef, memo } from "react";
 
 import './Tooltip.css';
 import useShowTransition from "Hooks/useShowTransition";
 import buildClassName from "Util/buildClassName";
 
-const Tooltip = ({
+const Tooltip = forwardRef(({
 	children,
 	style = undefined,
 	isOpen = false,
 	onCloseEnd = void 0,
-}) => {
+	positionX,
+}, ref) => {
 	const { transitionClassNames } = useShowTransition(isOpen, onCloseEnd, false, undefined, false, undefined, 100);
 	return (
-		<div className={buildClassName("tooltip", transitionClassNames)} style={style}>
+		<div className={buildClassName("tooltip", positionX, transitionClassNames)} style={style} ref={ref}>
 			<div className="pointer" />
 			<div className="content">{children}</div>
 		</div>
 	);
-};
+});
 
 export default memo(Tooltip);
