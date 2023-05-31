a25dda7e21e0b02e2044e89e164267c3aa4e72ed
MakAndJo <68428951+MakAndJo@users.noreply.github.com>
2023-05-31 17:27:01 +0500

    Update RangeSlider to FC

--- src/render/ui/components/Slider.js
+++ src/render/ui/components/Slider.js
@@ -1,82 +1,83 @@
-import { Component, createElement, createRef } from "react";
+import { createElement, createRef, useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { getPos } from "Libs/ElementEx.js";
 import style from "CSS/slider.module.css";
 import { randomString } from "Util/Random.js";
 import { toFixedNumber } from "Util/Numbers.js";
 import Tooltip from "./Tooltip.js";
 
-export default class RangeSlider extends Component {
+export default function RangeSlider({
+	id = undefined,
+	value = 0,
+	min = 0,
+	max = 100,
+	step = 1,
+	onChange = void 0,
+	unit = '',
+}) {
 
-	constructor(props) {
-		super(props);
-		props.id = props.id || randomString(5);
-		props.value = props.value || 0;
-		props.min = props.min || 0;
-		props.max = props.max || 100;
-		props.step = props.step || 1;
-		this.state = {
-			value: props.value,
-			notch: ((props.value - this.meta.min) / this.meta.step) * this.meta.inc,
-			popupOpen: false,
-		};
-		this.track = createRef();
-		this.grabber = createRef();
-	}
+	id = id || `slider-${randomString(5)}`;
 
-	get meta() {
+	const meta = useMemo(() => {
 		const val = {
-			max: Number(this.props.max),
-			min: Number(this.props.min),
-			step: Number(this.props.step),
+			max: Number(max),
+			min: Number(min),
+			step: Number(step),
 		};
 		val.ticks = (val.max - val.min) / val.step;
 		val.inc = 100 / val.ticks;
 		return val;
-	}
+	}, [max, min, step]);
 
+	const valRef = useRef(value);
+	const [currentNotch, setCurrentNotch] = useState(((value - meta.min) / meta.step) * meta.inc);
+	// const [popupOpen, setPopupOpen] = useState(false);
 
-	handleMousedown = (e) => {
-		document.addEventListener("mouseup", this.handleMouseup);
-		document.addEventListener("mousemove", this.handleMousemove);
-		this.handleMousemove(e);
-	};
+	const trackRef = useRef(undefined);
+	const grabberRef = useRef(undefined);
 
-	handleMousemove = (e) => {
-		const pos = getPos(this.track.current);
+	const handleMousemove = useCallback((e) => {
+		const pos = getPos(trackRef.current);
 		const diff = e.pageX - pos.left;
-		const percent = Math.max(Math.min(toFixedNumber((diff / this.track.current.offsetWidth) * 100, 1), 100), 0);
-		const notch = Math.max(Math.min(toFixedNumber(percent / this.meta.inc, 0) * this.meta.inc, 100), 0);
-		if (Math.abs(percent - notch) < this.meta.inc / 2) {
-			const value = Math.round((this.meta.min + (this.meta.step * (notch / this.meta.inc))) * 100) / 100;
+		const percent = Math.max(Math.min(toFixedNumber((diff / trackRef.current.offsetWidth) * 100, 1), 100), 0);
+		const notch = Math.max(Math.min(toFixedNumber(percent / meta.inc, 0) * meta.inc, 100), 0);
+		if (Math.abs(percent - notch) < meta.inc / 2) {
+			const value = Math.round((meta.min + (meta.step * (notch / meta.inc))) * 100) / 100;
 			const event = new MouseEvent("change", {
 				bubbles: false,
 				cancelable: true
 			});
-			let cancelled = !this.track.current.dispatchEvent(event);
+			let cancelled = !trackRef.current.dispatchEvent(event);
 			if (!cancelled) {
-				this.setState({ notch, value });
+				valRef.current = value;
+				setCurrentNotch(notch);
 			}
 		}
-	};
+	}, [meta]);
 
-	handleMouseup = () => {
-		document.removeEventListener("mouseup", this.handleMouseup);
-		document.removeEventListener("mousemove", this.handleMousemove);
-		if (typeof this.props.onChange === "function") {
-			this.props.onChange.call(this, this.state.value);
+	const handleMouseup = useCallback(() => {
+		document.removeEventListener("mouseup", handleMouseup);
+		document.removeEventListener("mousemove", handleMousemove);
+		if (typeof onChange === "function") {
+			onChange.call(this, valRef.current);
+			console.debug(">>>", valRef.current);
 		}
-	};
+	}, [handleMousemove, onChange]);
 
-	render(props, state) {
-		return (
-			<div className={style.slider} id={props.id}>
-				<div className={style.bar} style={{ width: `${state.notch}%` }} />
-				<div className={style.track} onMouseDown={this.handleMousedown} ref={this.track}>
-					<div className={style.grabber} ref={this.grabber} style={{ left: `${state.notch}%` }} />
-				</div>
-				<Tooltip forRef={this.grabber}>{state.value + this.props.unit}</Tooltip>
+	const handleMousedown = useCallback((e) => {
+		document.addEventListener("mouseup", handleMouseup);
+		document.addEventListener("mousemove", handleMousemove);
+		handleMousemove(e);
+	}, [handleMousemove, handleMouseup]);
+
+	return (
+		<div className={style.slider} id={id}>
+			<div className={style.bar} style={{ width: `${currentNotch}%` }} />
+			<div className={style.track} onMouseDown={handleMousedown} onMouseUp={handleMouseup} ref={trackRef}>
+				<div className={style.grabber} ref={grabberRef} style={{ left: `${currentNotch}%` }} />
 			</div>
-		);
-	}
+			<Tooltip forRef={grabberRef}>{valRef.current + unit}</Tooltip>
+		</div>
+	);
+
 
 }
