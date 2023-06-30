f4f911f2d8b63c8c8467817a3831103094b3f015
MakAndJo <68428951+MakAndJo@users.noreply.github.com>
2023-06-30 16:45:24 +0500

    Update radio

--- src/render/ui/Radio/RadioItem.tsx
+++ src/render/ui/Radio/RadioItem.tsx
@@ -1,15 +1,16 @@
 import { createElement, memo } from "react";
 import { randomString } from "Util/Random";
 
-import { SVG } from "UI/svg";
+
 import style from "./radio.module.css";
+import buildClassName from "Util/buildClassName";
 
 const RadioItem = ({ id, checked, onClick, children }) => {
 	id = id || randomString(5);
 	return (
 		<div className={style.item} data-id={id} aria-checked={checked} onClick={onClick}>
 			<div className={style.radioBar}>
-				<div className={style.radio}>{ checked ? SVG('radio-checked') : SVG('radio') }</div>
+				<div className={buildClassName(style.radio, checked && style.checked)}></div>
 				<div className={style.info}>{children}</div>
 			</div>
 		</div>
