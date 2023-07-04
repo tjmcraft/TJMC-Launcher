769dab5c067c3e24a56e63685284932dad3507ae
MakAndJo <68428951+MakAndJo@users.noreply.github.com>
2023-07-04 18:40:26 +0500

    Update TransitionContainer

--- src/render/ui/Transition/index.tsx
+++ src/render/ui/Transition/index.tsx
@@ -1,11 +1,9 @@
 import usePrevious from "Hooks/usePrevious";
-import useShowTransition from "Hooks/useShowTransition";
 import buildClassName from "Util/buildClassName";
-import forceReflow from "Util/forceReflow";
-// import forceReflow from "Util/forceReflow";
-import { createElement, FC, ReactNode, useLayoutEffect, useRef } from "react";
+import { createElement, useRef } from "react";
 
 import "./Transition.css";
+import TransitionContainer from "UI/TransitionContainer";
 
 export type ChildrenFn = (isActive: boolean, isFrom: boolean, currentKey: number) => React.ReactNode;
 export type TransitionProps = {
@@ -15,7 +13,9 @@ export type TransitionProps = {
   children: React.ReactNode | ChildrenFn;
 };
 
-const Transition: FC<TransitionProps> = ({
+const TRANSITION_DURATION = 250;
+
+const Transition: React.FC<TransitionProps> = ({
   activeKey,
   renderCount,
   className,
@@ -33,24 +33,24 @@ const Transition: FC<TransitionProps> = ({
 
   rendersRef.current[activeKey] = children;
 
-  useLayoutEffect(() => {});
-
   const renders = rendersRef.current;
   const renderKeys = Object.keys(renderCount ? new Array(renderCount).fill(undefined) : renders).map(Number);
   const contents = renderKeys.map((key) => {
     const render = renders[key];
-    if (!render) {
-      return undefined;
-    }
-
+    if (!render) return undefined;
     return (
-      <TransitionElement key={key} isActive={key === activeKey} firstMount={renderKeys.length <= 1} className={"trans-" + key} >
+      <TransitionContainer key={key}
+        duration={TRANSITION_DURATION}
+        isShown={key === activeKey}
+        noOpenTransition={renderKeys.length <= 1}
+        className={buildClassName("TransitionElement", "trans-" + key)}
+      >
         {
           typeof render === 'function'
             ? render(key === activeKey, key === prevActiveKey, activeKey)
             : render
         }
-      </TransitionElement>
+      </TransitionContainer>
     );
   });
 
@@ -62,30 +62,3 @@ const Transition: FC<TransitionProps> = ({
 };
 
 export default Transition;
-
-const TRANSITION_DURATION = 250;
-
-const TransitionElement = ({ children, className, isActive, firstMount }: {
-  children: ReactNode;
-  className?: string;
-  isActive: boolean;
-  firstMount: boolean;
-}) => {
-
-  const ref = useRef();
-
-  const {
-		shouldRender,
-		transitionClassNames,
-	} = useShowTransition(
-		isActive, undefined, firstMount, undefined, false, { }, TRANSITION_DURATION, className
-    );
-
-  if (ref.current) forceReflow(ref.current);
-
-	return shouldRender && (
-		<div className={buildClassName("TransitionElement", transitionClassNames, className)} ref={ref}>
-			{children}
-		</div>
-	);
-};
--- src/render/ui/TransitionContainer.tsx
+++ src/render/ui/TransitionContainer.tsx
@@ -1,17 +1,47 @@
-import { createElement, memo } from "react";
+import { createElement, memo, useRef } from "react";
 import buildClassName from "Util/buildClassName";
 import useShowTransition from "Hooks/useShowTransition";
+import forceReflow from "Util/forceReflow";
 
-const TransitionContainer = ({ isShown, className = undefined, duration = 350, children }) => {
+type OwnProps = {
+	children: React.ReactNode;
+	className?: string;
+	isShown: boolean;
+	duration?: number;
+	noOpenTransition?: boolean;
+	noCloseTransition?: boolean;
+	onCloseTransitionEnd?: NoneToVoidFunction;
+};
+
+const TransitionContainer: React.FC<OwnProps> = ({
+	children,
+	className = undefined,
+	isShown = false,
+	duration = 350,
+	noOpenTransition = false,
+	noCloseTransition = false,
+	onCloseTransitionEnd = void 0,
+}) => {
+
+	const containerRef = useRef<HTMLDivElement>();
+
 	const {
 		shouldRender,
 		transitionClassNames,
 	} = useShowTransition(
-		isShown, undefined, true, undefined, false, { }, duration
+		isShown,
+		onCloseTransitionEnd,
+		noOpenTransition,
+		undefined,
+		noCloseTransition,
+		{},
+		duration
 	);
 
+	if (containerRef.current) forceReflow(containerRef.current);
+
 	return shouldRender && (
-		<div className={buildClassName(className, transitionClassNames)}>
+		<div ref={containerRef} className={buildClassName(className, transitionClassNames)}>
 			{children}
 		</div>
 	);
