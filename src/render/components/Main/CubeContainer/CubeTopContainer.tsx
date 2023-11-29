c98ec0d2889d8804e7e293eff49bf49de73e64f8
MakAndJo <68428951+MakAndJo@users.noreply.github.com>
2023-11-29 23:21:42 +0500

    Update background

--- src/render/components/Main/CubeContainer/CubeTopContainer.tsx
+++ src/render/components/Main/CubeContainer/CubeTopContainer.tsx
@@ -1,12 +1,10 @@
 import { memo, createElement } from "react";
 import CubeTopToolbar from "./CubeTopToolbar";
 
-const imageLink = "https://cdn.tjmc.ru/images/1042-1920x1080.jpg";
-
 const CubeTopContainer = ({ hash }) => {
 	return (
 		<div className="top">
-			<img src={imageLink} />
+			<img src={require('IMG/1042-1920x1080.jpg')} />
 			<div className="top-overlay">
 				<CubeTopToolbar hash={hash} />
 			</div>
