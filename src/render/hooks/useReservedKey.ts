41fe459acb537fa58ddb0a9298da9eabf91913ea
MakAndJo <68428951+MakAndJo@users.noreply.github.com>
2023-11-29 18:01:06 +0500

    Created new hook useReservedKey

---
+++ src/render/hooks/useReservedKey.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import { getHotkeyMatcher } from 'Util/parseHotkeys';
+
+const IGNORE_TAGS = new Set(['INPUT', 'TEXTAREA', 'SELECT']);
+
+export default function useReservedKey(hotkey) {
+  const [isPressed, setIsPressed] = useState(false);
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (!shouldFireEvent(e)) {
+        return;
+      }
+      if (getHotkeyMatcher(hotkey)(e)) {
+        setIsPressed(true);
+      }
+    };
+    const handleKeyUp = (e: KeyboardEvent) => {
+      if (!shouldFireEvent(e)) {
+        return;
+      }
+      if (getHotkeyMatcher(hotkey)(e)) {
+        setIsPressed(false);
+      }
+    };
+    document.documentElement.addEventListener('keydown', handleKeyDown);
+    document.documentElement.addEventListener('keyup', handleKeyUp);
+    return () => {
+      document.documentElement.removeEventListener('keydown', handleKeyDown);
+      document.documentElement.removeEventListener('keyup', handleKeyUp);
+    }
+  }, [hotkey]);
+  return isPressed;
+}
+
+function shouldFireEvent(e: KeyboardEvent) {
+  if (e.target instanceof HTMLElement) {
+    return !IGNORE_TAGS.has(e.target.tagName);
+  }
+  return true;
+}
