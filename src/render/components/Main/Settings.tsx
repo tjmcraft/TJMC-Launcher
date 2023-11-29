e128e527c5c99f7f1936faf66253164f8afd5854
MakAndJo <68428951+MakAndJo@users.noreply.github.com>
2023-11-29 23:21:33 +0500

    Update avatars render

--- src/render/components/Main/Settings.tsx
+++ src/render/components/Main/Settings.tsx
@@ -196,7 +196,7 @@ const MyAccountTab = memo(() => {
 									backgroundImage: user.avatar != void 0 ?
 										`url(https://cdn.tjmc.ru/avatars/${user.id}/${user.avatar}.png?size=256)` :
 										`url(https://api.tjmc.ru/v1/skin.render?user=${user.username}&headOnly=true&vr=-25&hr=35)`
-								}} className="accountAvatar" />
+								}} className={buildClassName("accountAvatar", "bimAvatar")} />
 							</div>
 							<div className={buildClassName("flex-group", "vertical")}>
 								<span className={buildClassName("vbx", "cu")}>
@@ -242,7 +242,7 @@ const SkinTab = memo(() => {
 	const user = useGlobal(selectCurrentUser);
 
 	const handleChangeClick = useCallback(() => {
-		window.open(`https://www.tjmc.ru/login/skinch.php`);
+		window.open(`https://id.tjmc.ru/client/skin`);
 	}, []);
 
 	const handleDownloadClick = useCallback(() => {
--- src/render/components/Main/UserPanel.tsx
+++ src/render/components/Main/UserPanel.tsx
@@ -7,6 +7,7 @@ import useGlobal from "Hooks/useGlobal";
 
 import Tooltip from "UI/Tooltip";
 import buildClassName from "Util/buildClassName";
+import UserIcon from "./UserIcon";
 
 const UserPanel: React.FC<{
 	children?: React.ReactNode;
@@ -17,13 +18,7 @@ const UserPanel: React.FC<{
 		<div className="panel">
 			<div className="container">
 				<div className="avatarWrapper" onClick={() => openSettings({ tab: 'my-account' })}>
-					<div className="avatar">
-						{(user.avatar != void 0) ? (
-							<img src={`https://cdn.tjmc.ru/avatars/${user.id}/${user.avatar}.png?size=64`} />
-						) : (
-							<img src={`https://api.tjmc.ru/v1/skin.render?user=${user.username}&headOnly=true&vr=-25&hr=35`} />
-						)}
-					</div>
+					<UserIcon />
 					<div className="nameTag">
 						<div className="title">{user.realname || user.username}</div>
 						<div className="subtitle">{`#${user.discriminator}`}</div>
